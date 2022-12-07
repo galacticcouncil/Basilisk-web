e4fd8fb69514969f8818017bf31da72607f4ce12
Darken141 <Darken141@gmail.com>
2022-12-07 16:49:38 +0100

    update config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,30 @@ const nextConfig = {
 	compiler: {
 		styledComponents: true,
 	},
+	async headers() {
+		return [
+			{
+				source: "/(.*)?", // Matches all pages
+				headers: [
+					{
+						key: "X-Frame-Options",
+						value: "DENY",
+					},
+					{
+						key: "X-XSS-Protection",
+						value: "1; mode=block",
+					},
+					{
+						key: "Content-Security-Policy",
+						value:
+							"default-src 'self'; script-src 'self'; style-src 'self'; font-src 'self';"
+								.replace(/\s{2,}/g, " ")
+								.trim(),
+					},
+				],
+			},
+		]
+	},
 }
 
 module.exports = nextConfig
