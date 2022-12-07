d2764d26c5c2003f28ef771e7efca67337cfe827
Darken141 <Darken141@gmail.com>
2022-12-07 17:08:38 +0100

    update config

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ const nextConfig = {
 					{
 						key: "Content-Security-Policy",
 						value:
-							"default-src 'self'; script-src 'self'; style-src 'self'; font-src 'self';"
+							"default-src 'self' 'unsafe-eval' 'unsafe-inline' api.fontshare.com; frame-ancestors 'none'; font-src 'self' 'unsafe-eval' ; connect-src 'self'"
 								.replace(/\s{2,}/g, " ")
 								.trim(),
 					},
